68c9b78dc05cab890383545d58e5790f3e73df70
InsituPeter <insitupeter@gmail.com>
2025-02-07 22:29:42 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const express= require("express")
+const cors = require("cors")
+const app= express()
+const axios = require("axios")
+const PORT=process.env.PORT|| 3000
+//const number_Api = require("./route")
+
+//Middleware
+app.use(cors())
+app.use(express.json())
+//app.use("api/v1", number_Api)
+
+
+const isPrime=(num)=>{
+    if (num<2) return false;
+    for(let i=2; i<=Math.sqrt(num); i++){
+        if(num % i===0)
+            return false
+    }
+    return true;
+
+}
+
+
+const isPerfect=(num)=>{
+   let sum=0;
+  for (let i=1 ; i<num; i++){
+    if(num % i===0)
+      sum +=i;
+  }
+  return sum === num;
+}
+
+const isArmstrong =(num)=>{
+   const digits= num.toString().split("").map(Number)
+   const power = digits.length;
+   const sum = digits.reduce((acc, d )=> acc + Math.pow(d, power), 0);
+   return sum=== num;
+}
+
+const digitSum =(num)=>{
+    return num.toString().split("").reduce((acc, d)=> acc + parseInt(d), 0)
+}
+
+
+
+app.get("/" , async(req, res)=>{
+    const number= req.query.number
+    const num = parseInt(number)
+
+    if (isNaN(num)){
+        return res.status(400).json({number, error:true})
+    }
+
+
+    try{
+    const funfactResponse = await axios.get(`http://numbersapi.com/${num}?json`)
+    const funFact= funfactResponse.data.text;
+    res.status(200).json({
+      number : num,
+      is_prime:isPrime(num),
+      is_perfect:isPerfect(num),
+      properties:[isArmstrong(num) ? "armstrong":"", num%2===0? "even": "odd"].filter(Boolean),
+      digit_sum:digitSum(num),
+      fun_fact:funFact
+    })
+}
+catch(error){
+   res.status(500).json({error:"failed to fetch fun fact"})
+}
+})
+
+
+
+
+
+
+app.listen(PORT, ()=>console.log(`Server is listening on ${PORT}`))
